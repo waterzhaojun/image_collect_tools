cabcecd143517961c6b429917410ae4e7acb78a6
waterzhaojun <zhaojunwater@gmail.com>
2018-10-16 21:48:41 -0400

    Add files via upload

--- pi_webcam_record_v3.py
+++ pi_webcam_record_v3.py
@@ -8,35 +8,38 @@ Created on Sat Oct 13 09:13:00 2018
 
 import numpy as np
 import cv2
-import scipy
+#import imutils
 #import argparse
 
 def main():
-    
 
-    cap_0 = cv2.VideoCapture(0)
-    cap_1 = cv2.VideoCapture(1)
+    width = 200
+    height = 150
+
+    cap = cv2.VideoCapture(0)
+    # cap.set(cv2.CAP_PROP_FPS, 1)
+    # cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+    # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
     while(True):
         # Capture frame-by-frame
-        ret_0, frame_0 = cap_0.read()
-        ret_1, frame_1 = cap_1.read()
-        
-        frame_0 = scipy.misc.imresize(frame_0, 0.5)
-        frame_1 = scipy.misc.imresize(frame_1, 0.5)
-        # Our operations on the frame come here
-        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        frame = np.concatenate((frame_0, frame_1), axis = 0)
-    
-        # Display the resulting frame
-        cv2.imshow('frame', frame)
+        ret, frame = cap.read()
+
+        if ret:
+            
+        else:
+            print('Not ready')
+
+#       Display the resulting frame
+            cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     
     # When everything done, release the capture
     cap_0.release()
     cap_1.release()
+    cap_2.release()
+    cap_3.release()
     cv2.destroyAllWindows()
     
 main()
